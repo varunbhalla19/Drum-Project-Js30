69ace28f6064918848baab35b2b4f992b7e37b3a
varunbhalla19 <varunbhalla19@gmail.com>
2020-09-16 10:30:51 +0530

    Some changes

--- drum2.js
+++ drum2.js
@@ -1,30 +1,33 @@
 const keys = document.querySelectorAll('.key.op');
 const mainKeys = document.querySelectorAll('.main');
 const btn = document.querySelector('.play');
-const btn_cover = document.querySelector('.cover-btn') ; 
+const btn_cover = document.querySelector('.cover-btn');
 const pause = document.querySelector('.reset');
 const range = document.getElementById('range');
 
+const windowWidth = window.innerWidth;
 
-let speed = 300, seqIndex = 0, intervalId  , paused = 0    ; 
+const small = windowWidth <= 600 ? true : false;
+
+let speed = 300, seqIndex = 0, intervalId, paused = 0;
 
 // play button
 btn.addEventListener('click', e => {
-    play() ;
-    btn_cover.classList.add('show') ;
+    play();
+
 });
 
 // pause button
 pause.addEventListener('click', e => {
-    console.log("reset clicked");
-    paused = 1 ;
-    btn_cover.classList.remove('show') ;
+    // console.log("reset clicked");
+    paused = 1;
+    btn_cover.classList.remove('show');
     clearInterval(intervalId);
 });
 
 // range slider to adjust speed...
 range.addEventListener('input', ev => {
-    speed = 600 -  range.value
+    speed = 600 - range.value
 });
 
 
@@ -33,12 +36,12 @@ range.addEventListener('input', ev => {
 keys.forEach(el => {
     el.addEventListener('click', ev => {
 
-        if( paused ) {
+        if (paused) {
 
             // el.classList.toggle('selected' ) ;            // el.classList.toggle('disabled') ;
 
-            if(el.classList.contains('disabled')) {
-                el.classList.add('rz') ;
+            if (el.classList.contains('disabled')) {
+                el.classList.add('rz');
             }
             // return ;
         }
@@ -53,7 +56,7 @@ keys.forEach(el => {
 // play individual sounds on leftmost keys
 mainKeys.forEach(el => {
     el.addEventListener('click', (ev) => {
-        let atr = el.getAttribute('data-key') ;
+        let atr = el.getAttribute('data-key');
         // console.log({ el }, atr);
         let audio = document.querySelector(`audio[data-key="${atr}"]`);
         audio.currentTime = 0;
@@ -64,9 +67,13 @@ mainKeys.forEach(el => {
 
 // play with selected array
 
+let tempResult ;
+
 function play(e) {
 
-    paused = 0 ;
+    tempResult = 0 ;
+
+    paused = 0;
 
     let sequence = [];     // 2D array , contains selected sounds for each column....
 
@@ -77,6 +84,8 @@ function play(e) {
         sequence[i - 1] = [];
 
         selected = Array.from(document.querySelectorAll(`.selected[data-col="${i}"]`));       // select selected elements from each column....
+
+        tempResult += selected.length ;
 
         selected.forEach(el => {
             key = el.getAttribute('data-key');
@@ -90,29 +99,43 @@ function play(e) {
 }
 
 
-
+let roundCounter = 0 ;
 
 // play sounds
 function playSound(sequence, ans) {
+
+    // console.log(sequence , tempResult ) ;
+
+
+
+    if(tempResult == 0) {
+        // console.log("Length 0 , dont play!!")
+        return ;
+    }
+
+    btn_cover.classList.add('show');
 
     intervalId = setInterval(() => {
 
         if (seqIndex >= 8) {
             seqIndex = 0;
+            // console.log(seqIndex) ;
+            roundCounter++ ;
         };
 
         sequence[seqIndex].forEach(sounds => {
             sounds.music.currentTime = 0;
 
             // remove rotation
-            sounds.elem.classList.remove('rz' );
-            
-            
-            if (sounds.i++ % 2 == 0) {
-                sounds.elem.style.transform = 'rotate(5deg)'
-            } else {
-                sounds.elem.style.transform = 'rotate(-5deg)'
-            }
+            sounds.elem.classList.remove('rz');
+
+                if (roundCounter % 2 == 0) {
+                    sounds.elem.style.transform =  ( small ? 'scale(0.9)' : 'rotate(3deg)' )
+                } else {
+                    sounds.elem.style.transform = ( small ? 'scale(1.05)' : 'rotate(-3deg)') 
+                }
+            // } 
+
             sounds.music.play()
         });
         seqIndex++;
